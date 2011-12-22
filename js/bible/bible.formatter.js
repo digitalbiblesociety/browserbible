59863dc6e85117a4a466da768f7ab68279c33d25
John Dyer <johndyer@gmail.com>
2011-12-22 12:01:12 -0600

    univeral bible code

--- js/bible/bible.formatter.js
+++ js/bible/bible.formatter.js
@@ -16,17 +16,67 @@ bible.BibleFormatter = {
 		return 'c' + this.padLeft((bookNumber).toString(), 3, '0') + this.padLeft((chapterNumber).toString(), 3, '0');
 	},
 	
-	parseVerseCode: function(verseCode, bookNameIndex) {
+	generateVerseCode: function(bookNumber, chapterNumber, verseNumber) {
+		return 'v' + this.padLeft((bookNumber).toString(), 3, '0') + this.padLeft((chapterNumber).toString(), 3, '0') + this.padLeft((verseNumber).toString(), 3, '0');
+	},	
+	
+	parseBibleCode: function(bibleCode, bookNameIndex) {
+		if (bibleCode.length != 7 && bibleCode.length != 10)
+			return null;
+		
+		// book, chapter, verse
+		return {
+			bookNumber: parseInt(bibleCode.substring(1,4),10),
+			chapterNumber: parseInt(bibleCode.substring(4,7),10),
+			verseNumber: bibleCode.length == 10 ? parseInt(bibleCode.substring(7,10),10) : 0
+		};
+	},
+	
+	verseCodeToReferenceString: function(verseCode, bookNameIndex) {
 		if (verseCode.length != 10)
 			return 'unknown';
 		
 		if (typeof bookNameIndex == 'undefined')
 			bookNameIndex = 2;
+			
+		var referenceData = this.parseBibleCode(verseCode);
 		
-		var bookNumber = parseInt(verseCode.substring(1,4),10),
-			chapterNumber = parseInt(verseCode.substring(4,7),10),
-			verseNumber = parseInt(verseCode.substring(7,10),10);
+		return bible.Books[referenceData.bookNumber-1].names[bookNameIndex] + ' ' + referenceData.chapterNumber + ':' + referenceData.verseNumber;
+	},	
+	
+	getNextChapterCode: function(chapterCode) {
+		var referenceData = this.parseBibleCode(chapterCode);
 		
-		return bible.Books[bookNumber-1].names[bookNameIndex] + ' ' + chapterNumber + ':' + verseNumber;
-	}	
+		// if at the end of the current book
+		if (referenceData.chapterNumber == bible.Books[referenceData.bookNumber-1].verses.length) {
+			
+			// if this is the last book
+			if (referenceData.bookNumber == bible.Books.length) {
+				return null;
+			} else {
+				return this.getChapterCode(referenceData.bookNumber+1, 1);
+			}
+			
+		} else {	
+			return this.getChapterCode(referenceData.bookNumber, referenceData.chapterNumber+1);
+		}
+	},
+	
+	getPrevChapterCode: function(chapterCode) {
+		var referenceData = this.parseBibleCode(chapterCode);
+
+		// if at the beginning of the current book
+		if (referenceData.chapterNumber == 1) {
+			
+			// if this is the first book
+			if (referenceData.bookNumber == 1) {
+				return null;
+			} else {
+				return this.getChapterCode(referenceData.bookNumber-1, bible.Books[referenceData.bookNumber-2].verses.length);
+			}
+			
+		} else {	
+			return this.getChapterCode(referenceData.bookNumber, referenceData.chapterNumber-1);
+		}
+	}
 };

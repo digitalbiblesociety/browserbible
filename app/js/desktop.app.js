90ed62c1bd8816f10fda18324ea77ef596f7af31
John Dyer <johndyer@gmail.com>
2012-01-03 17:08:13 -0600

    added fourth column

--- app/js/desktop.app.js
+++ app/js/desktop.app.js
@@ -12,6 +12,8 @@ jQuery(function($) {
 	docs.DocManager.addDocument(bible.BibleNavigator, 'en_kjv');
 	docs.DocManager.addDocument(bible.BibleNavigator, 'es_rv');
 	docs.DocManager.addDocument(bible.BibleNavigator, 'ar_svd');
+	docs.DocManager.addDocument(bible.BibleNavigator, 'zhcn_ncv');
+	
 	
 	docs.DocManager.documents[0].navigateById('Rom.1.1', true);
 	

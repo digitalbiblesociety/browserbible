ce2a199f770d77e2c2e46409e0f9e1282747fd6d
John Dyer <johndyer@gmail.com>
2012-01-07 15:33:49 -0600

    rebuild of strong's with amazing data from openscriptures

--- app/js/docs/plugins.highlighers.morphology.js
+++ app/js/docs/plugins.highlighers.morphology.js
@@ -13,19 +13,45 @@ docs.plugins.push({
 			
 			var word = $(this),
 				lemma = word.attr('data-lemma'),
-				morph = word.attr('data-morph');
+				lemmaParts = lemma != null ? lemma.split(' ') : [],
+				morph = word.attr('data-morph'),
+				morphParts = morph != null ? morph.split(' ') : [],
+				displayTextArray = [],
+				strongLetter = '',
+				strongInfo = '',
+				strongKey = '',
+				strongNumber = 0, 
+				strongData = {}; 
+			
+			if (lemmaParts.length > 0) {
 				
-			morph = morph.split(' ')[0].replace('robinson:','');
+				for (var i=0, il=lemmaParts.length; i<il; i++) {
+					strongInfo = lemmaParts[i].replace('strong:','');
+					strongLetter = strongInfo.substring(0,1);
+					strongNumber = parseInt(strongInfo.substring(1), 10);
+					strongKey = strongLetter + strongNumber.toString();
+					morph = (i< morphParts.length) ? morphParts[i].replace('robinson:','') : '';
+						
+					if (strongLetter == 'H')
+						strongData = strongsHebrewDictionary[strongKey];
+					else if (strongLetter == 'G')
+						strongData = strongsGreekDictionary[strongKey];
+					
+					if (typeof strongData != 'undefined') {
+						displayTextArray.push( strongData.lemma + ' (' + strongKey + ') - ' + strongData.strongs_def + (strongLetter == 'G' ? ' [' + bible.morphology.Greek.getMorphology( morph ) + ']' : '') );
+					}
+					
+					
+				}
 				
-			// shown in footer
-			//if (lex != null && morph != null && lex.substring(0,8) == 'strong:G') {	
-			if (morph != null) {
 				word
 					.closest('.document-container')
 					.find('.document-footer')
-					.html(lemma + ' - ' + bible.morphology.Greek.getMorphology( morph ) ); //+ ' (' + morph + ')');
-			}			
+					.html(displayTextArray.join('; '));
+						
 			
+			}
+					
 		});
 		
 	}

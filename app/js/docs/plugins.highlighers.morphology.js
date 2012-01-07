366df718a7106b51ce952594a110909935f12d09
John Dyer <johndyer@gmail.com>
2012-01-06 23:19:26 -0600

    greek morphology is displayed again

--- app/js/docs/plugins.highlighers.morphology.js
+++ app/js/docs/plugins.highlighers.morphology.js
@@ -12,15 +12,18 @@ docs.plugins.push({
 		content.delegate('span.word', 'mouseover', function() {
 			
 			var word = $(this),
-				lex = word.attr('data-lex'),
+				lemma = word.attr('data-lemma'),
 				morph = word.attr('data-morph');
 				
+			morph = morph.split(' ')[0].replace('robinson:','');
+				
 			// shown in footer
-			if (lex != null && morph != null && lex.substring(0,4) == 'sn:G') {	
+			//if (lex != null && morph != null && lex.substring(0,8) == 'strong:G') {	
+			if (morph != null) {
 				word
 					.closest('.document-container')
 					.find('.document-footer')
-					.html('' + bible.morphology.Greek.getMorphology( morph ) );
+					.html(lemma + ' - ' + bible.morphology.Greek.getMorphology( morph ) ); //+ ' (' + morph + ')');
 			}			
 			
 		});

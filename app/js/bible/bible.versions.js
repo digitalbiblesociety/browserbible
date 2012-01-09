1b7d1d74ff8d834da855ed53c7ca57e84003d7fb
John Dyer <johndyer@gmail.com>
2012-01-09 08:03:00 -0600

    support for dynamically loaded version information

--- app/js/bible/bible.versions.js
+++ app/js/bible/bible.versions.js
@@ -1,4 +1,88 @@
 bible.versions = {
+	// prebuild version array
+	versionData: null,
+	
+	allVersions: [
+		'ar_svd',
+		'en_esv',
+		'en_kjv',
+		'en_nasb',
+		'en_net',
+		'en_oeb',
+		'en_web',
+		'el_tisch',
+		'es_rv',
+		'he_wlc',
+		'ru_syn',
+		'tr_turk',
+		'zhcn_ncv',
+		'zhtw_ncv'
+	],
+	
+	loadingVersionIndex: -1,
+	
+	loadingCallback: null,
+	
+	loadNextVersion: function() {
+		
+		var t = this,
+			versionFolder = '';
+		
+		t.loadingVersionIndex++;
+		
+		if (t.loadingVersionIndex < t.allVersions.length) {	
+		
+			versionFolder = this.allVersions[this.loadingVersionIndex];
+		
+			$.ajax({
+				url: 'content/bibles/' + versionFolder + '/version.json',
+				dataType: 'json',
+				success: function(data) {
+					
+					// create language
+					if (typeof t.versionData[data.language.toLowerCase()] == 'undefined') {
+						t.versionData[data.language.toLowerCase()] = {
+							languageName: bible.language.codes[data.language.toLowerCase()],
+							versions: {}
+						};
+					}
+					
+					// insertdata
+					t.versionData[data.language.toLowerCase()].versions[data.code] = data;
+					
+					t.loadNextVersion();
+				},
+				error: function() {
+					
+					t.loadNextVersion();
+				}
+			});
+		} else {
+			t.loadingFinished();
+		}
+		
+	},
+	
+	loadingFinished: function() {
+		//this.versionData = bible.versionData;
+	
+		this.loadingCallback(bible.versionData);		
+	},
+	
+	getVersions: function(callback) {
+	
+		this.loadingCallback = callback;
+		
+		if (this.versionData == null) {
+			this.versionData = {};
+			this.loadNextVersion();
+		} else {
+			this.loadingFinished();
+		}
+	}
+};
+	
+bible.versionData = {
 	'ar' : {
 		languageName: 'Arabic',
 		versions: {
@@ -122,3 +206,64 @@ bible.versions = {
 		}
 	}
 };
+
+bible.language = {
+		codes:  {
+			af:'Afrikaans',
+			sq:'Albanian',
+			ar:'Arabic',
+			be:'Belarusian',
+			bg:'Bulgarian',
+			ca:'Catalan',
+			zh:'Chinese',
+			'zh-cn':'Chinese Simplified',
+			'zh-tw':'Chinese Traditional',
+			hr:'Croatian',
+			cs:'Czech',
+			da:'Danish',
+			nl:'Dutch',
+			en:'English',
+			et:'Estonian',
+			tl:'Filipino',
+			fi:'Finnish',
+			fr:'French',
+			gl:'Galician',
+			de:'German',
+			el:'Greek',
+			ht:'Haitian Creole',
+			iw:'Hebrew',
+			hi:'Hindi',
+			hu:'Hungarian',
+			is:'Icelandic',
+			id:'Indonesian',
+			ga:'Irish',
+			it:'Italian',
+			ja:'Japanese',
+			ko:'Korean',
+			lv:'Latvian',
+			lt:'Lithuanian',
+			mk:'Macedonian',
+			ms:'Malay',
+			mt:'Maltese',
+			no:'Norwegian',
+			fa:'Persian',
+			pl:'Polish',
+			pt:'Portuguese',
+			//'pt-pt':'Portuguese (Portugal)',
+			ro:'Romanian',
+			ru:'Russian',
+			sr:'Serbian',
+			sk:'Slovak',
+			sl:'Slovenian',
+			es:'Spanish',
+			sw:'Swahili',
+			sv:'Swedish',
+			tl:'Tagalog',
+			th:'Thai',
+			tr:'Turkish',
+			uk:'Ukrainian',
+			vi:'Vietnamese',
+			cy:'Welsh',
+			yi:'Yiddish'
+		}
+	};

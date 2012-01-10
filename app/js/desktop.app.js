72717503b9192ea63b52741ca37e7cdd64d63508
John Dyer <johndyer@gmail.com>
2012-01-09 21:58:33 -0600

    add/close buttons

--- app/js/desktop.app.js
+++ app/js/desktop.app.js
@@ -33,41 +33,11 @@ jQuery(function($) {
 			}			
 			]
 		};
-
-		var docSettings = $.jStorage.get('docs-settings', defaultDocSettings);
+		docs.DocManager.loadSettings(defaultDocSettings);
 		
-		console.log(docSettings);
-		
-		
-		// setup all documents and load content
-		for (var i=0, il=docSettings.docs.length; i<il; i++) {
-			var docSetting = docSettings.docs[i],
-				document = docs.DocManager.addDocument(bible.BibleNavigator, docSetting.version);
-			
-			document.syncCheckbox.prop('checked', docSetting.linked);
-			document.navigateById(docSetting.location, false);
-		}
-		
-		docs.DocManager.addEventListener('navigation', function(e) {
-			
-			// save the state of all?
-			//console.log('nav event');
-			var docSettings = {docs:[]};
-			for (var i=0, il=docs.DocManager.documents.length; i<il; i++) {
-				var document = docs.DocManager.documents[i];
-				docSettings.docs.push({
-					version: document.selector.val(),
-					//location: document.input.val(),
-					location: document.fragmentId,
-					linked: document.syncCheckbox.prop('checked')
-				});
-			}
-			
-			//console.log(docSettings);
-			$.jStorage.set('docs-settings', docSettings);
+		docs.DocManager.addEventListener('navigation', function(e) {		
+			docs.DocManager.saveSettings();
 		});
-
-
 		
 		// startup plugins
 		var content = $('#content');
@@ -77,8 +47,18 @@ jQuery(function($) {
 		
 		// fullscren button
 		$('#docs-fullscreen').on('click', function() {
-			$('body')[0].webkitRequestFullScreen();
+			//$('body')[0].webkitRequestFullScreen();
+			$('body')[0].webkitRequestFullScreenWithKeys();
+			
 		});
+		
+		// fullscren button
+		$('#docs-add-document').on('click', function() {
+			
+			docs.DocManager.addDocument(bible.BibleNavigator, 'en_kjv');
+			docs.DocManager.documents[docs.DocManager.documents.length-1].navigateByString(docs.DocManager.documents[0].input.val());
+			docs.DocManager.saveSettings();
+		});		
 	});
 	
 	// search

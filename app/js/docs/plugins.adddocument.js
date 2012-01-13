5bf40188e590077c7363ef90fa3449c3c63d0433
John Dyer <johndyer@gmail.com>
2012-01-13 15:57:36 -0600

    moved top nav to plugins

---
+++ app/js/docs/plugins.adddocument.js
@@ -0,0 +1,22 @@
+	
+/**
+ * Option to add additional documents
+ *
+ * @author John Dyer (http://j.hn/)
+ */
+ 
+docs.plugins.push({
+
+	init: function(docManager) { 	
+	
+		$('<input type="button" id="docs-add-document" />')
+			.appendTo(docManager.header.find('#header-nav'))
+			.on('click', function() {
+			
+				docManager.addDocument(bible.BibleNavigator, 'en_kjv');
+				docManager.documents[docManager.documents.length-1].navigateByString(docManager.documents[0].input.val());
+				docManager.saveSettings();
+			});
+		
+	}
+});

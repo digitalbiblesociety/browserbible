95d56d4e64726a45072e20bd70f34805d6c68d7c
John Dyer <johndyer@gmail.com>
2012-02-06 11:05:09 -0600

    popup notes with timeout

--- app/js/docs/plugins.notes.js
+++ app/js/docs/plugins.notes.js
@@ -7,7 +7,37 @@
  
 docs.plugins.push({
 
-	init: function(docManager) { 	
+	init: function(docManager) {
+		
+		var notesPopup = docs.createModal('notes', 'Notes &amp; Cross References'),
+			timer = null,
+			selectedNote = null,
+			startTimer = function() {				
+				stopTimer();
+				console.log('starting notes timer');
+				timer = setTimeout(function() {
+					notesPopup.hide();
+					selectedNote = null;
+				}, 1000);
+			},
+			stopTimer = function() {
+				if (timer != null) {
+					clearTimeout(timer);
+					timer = null;
+				}
+			};
+			
+		notesPopup.content.css({'max-height': '200px', 'overflow': 'auto'});
+		
+		notesPopup.window
+			.on('mouseleave', function() {
+				startTimer();
+			})
+			.on('mouseover', function() {
+				stopTimer();	
+			})		
+		
+		
 	
 		// SHOW specific note in the footer
 		docManager.content.on('mouseover', 'span.note, span.cf', function() {
@@ -19,6 +49,7 @@ docs.plugins.push({
 				.find('.document-footer')
 				.empty()
 				.append(note.clone());
+			
 						
 		}).on('mouseout', 'span.note, span.cf', function() {
 			
@@ -26,6 +57,48 @@ docs.plugins.push({
 				.closest('.document-container')
 				.find('.document-footer')
 				.empty();
+		}).on('click', 'span.note, span.cf', function(e) {
+		
+			var note = $(this);
+			
+			if (selectedNote!= null && selectedNote.html() == note.html() && notesPopup.window.is(':visible')) {
+				notesPopup.hide();
+				return;
+			}
+			
+			selectedNote = note;
+		
+			if (note.hasClass('cf')) {
+				notesPopup.title.html('Cross Reference');
+			} else if (note.hasClass('note')) {
+				notesPopup.title.html('Note');
+			}
+		
+			notesPopup.content.empty().append(note.clone());
+
+			
+			// measure position
+			var 
+				notePos = note.offset(),
+				noteWidth = note.width(),
+				noteHeight = note.height(),
+				popupWidth = notesPopup.window.outerWidth(true),
+				popupHeight = notesPopup.window.outerHeight(true),
+				windowWidth = $(window).width(),
+				windowHeight = $(window).height(),
+				// default to below and to the right
+				top = (notePos.top + noteHeight + noteHeight > windowHeight) ? notePos.top - popupHeight - 5 : notePos.top + noteHeight + 4,
+				left = (notePos.left + popupWidth > windowWidth) ? windowWidth - popupWidth : notePos.left;			
+			
+			notesPopup.show();
+			notesPopup.window.css({top: top, left: left});
+			
+			stopTimer();
+			
+			$(document).one('mousemove', function() {
+				startTimer();
+			});			
+			
 		});
 		
 		// SHOW all notes from a verse in the footer

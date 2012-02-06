06d38e21a5241a08ea1dd7dcb7c5cc55bf2f239a
John Dyer <johndyer@gmail.com>
2012-02-06 11:06:12 -0600

    added verse and note removal

--- app/js/docs/plugins.config.js
+++ app/js/docs/plugins.config.js
@@ -37,7 +37,7 @@ docs.plugins.push({
 			.appendTo(document.body)
 			.hide();
 			*/
-			configWindow = docs.createModal('config', 'Configuration').size(400, 200);
+			configWindow = docs.createModal('config', 'Configuration').size(400, 300);
 			
 			
 		function createOptionSet(title, prefix, data) {
@@ -60,8 +60,9 @@ docs.plugins.push({
 					});
 		
 			for (var i=0, il=data.length; i<il; i++) {
-				var name = data[i].name;
-				configBlock.append($('<input type="radio" name="config-' + prefix + '-choice" id="config-' + prefix + '-' + name.toLowerCase() + '" value="' + name.toLowerCase() + '" /><label for="config-' + prefix + '-' + name.toLowerCase() + '">' + name + '</label>'))
+				var name = data[i].name,
+					value = name.toLowerCase().replace(' ','');
+				configBlock.append($('<input type="radio" name="config-' + prefix + '-choice" id="config-' + prefix + '-' + value + '" value="' + value + '" /><label for="config-' + prefix + '-' + value + '">' + name + '</label>'))
 			}
 			var userConfig = $.jStorage.get('docs-config-' + prefix, 'default');
 			
@@ -72,95 +73,9 @@ docs.plugins.push({
 		createOptionSet('Theme', 'theme', colorOptions);
 		createOptionSet('Fonts', 'font', fontOptions);
 		createOptionSet('Size', 'size', sizeOptions);
-		
-		/*
-		// create font options
-		var fontBlock =
-				$('<div class="config-options" id="config-fonts"><h3>Fonts</h3></div>')
-					.appendTo( configWindow.find('.modal-content') )
-					.on('click', 'input', function() {
-						console.log('clicked',this);
-						var bod = $(document.body);
-						
-						// remove all fonts
-						$(this).siblings('input').each(function(i, input) {
-							bod.removeClass('config-font-' + $(input).val() );
-						});
-						
-						// select this one
-						bod.addClass( 'config-font-' + $(this).val() );
-						
-						$.jStorage.set('docs-config-font', $(this).val());
-					});
-		
-		for (var i=0, il=fontOptions.length; i<il; i++) {
-			var title = fontOptions[i].name;
-			fontBlock.append($('<input type="radio" name="config-font-choice" id="config-font-' + title.toLowerCase() + '" value="' + title.toLowerCase() + '" /><label for="config-font-' + title.toLowerCase() + '">' + title + '</label>'))
-		}
-		var userFontConfig = $.jStorage.get('docs-config-font', 'default');
-		
-		fontBlock.find('#config-font-' + userFontConfig + '').trigger('click');
-		
-		//	'<div class="config-options" id="config-colors"><h3>Colors</h3></div>' +
-							
-		// create font options
-		var colorBlock =
-				$('<div class="config-options" id="config-colors"><h3>Theme Colors</h3></div>')
-					.appendTo( configWindow.find('.modal-content') )
-					.on('click', 'input', function() {
-						console.log('clicked',this);
-						var bod = $(document.body);
-						
-						// remove all fonts
-						$(this).siblings('input').each(function(i, input) {
-							bod.removeClass('config-theme-' + $(input).val() );
-						});
-						
-						// select this one
-						bod.addClass( 'config-theme-' + $(this).val() );
-						
-						$.jStorage.set('docs-config-theme', $(this).val());
-					});
-		
-		for (var i=0, il=colorOptions.length; i<il; i++) {
-			var title = colorOptions[i].name;
-			colorBlock.append($('<input type="radio" name="config-theme-choice" id="config-theme-' + title.toLowerCase() + '" value="' + title.toLowerCase() + '" /><label for="config-theme-' + title.toLowerCase() + '">' + title + '</label>'))
-		}
-		var userColorConfig = $.jStorage.get('docs-config-theme', 'default');
-		
-		colorBlock.find('#config-theme-' + userColorConfig + '').trigger('click');
-		
-		
-		
-		
-		// create font options
-		var sizeBlock =
-				$('<div class="config-options" id="config-size"><h3>Font Size</h3></div>')
-					.appendTo( configWindow.find('.modal-content') )
-					.on('click', 'input', function() {
-						console.log('clicked',this);
-						var bod = $(document.body);
-						
-						// remove all fonts
-						$(this).siblings('input').each(function(i, input) {
-							bod.removeClass('config-theme-' + $(input).val() );
-						});
-						
-						// select this one
-						bod.addClass( 'config-theme-' + $(this).val() );
-						
-						$.jStorage.set('docs-config-theme', $(this).val());
-					});
-		
-		for (var i=0, il=colorOptions.length; i<il; i++) {
-			var title = colorOptions[i].name;
-			sizeBlock.append($('<input type="radio" name="config-theme-choice" id="config-theme-' + title.toLowerCase() + '" value="' + title.toLowerCase() + '" /><label for="config-theme-' + title.toLowerCase() + '">' + title + '</label>'))
-		}
-		var userColorConfig = $.jStorage.get('docs-config-theme', 'default');
-		
-		sizeBlock.find('#config-theme-' + userColorConfig + '').trigger('click');		
-		*/
-		
+		createOptionSet('Verses', 'verses', [{name: 'Default'},{name: 'Hide Verses'}]);
+		createOptionSet('Notes', 'notes', [{name: 'Default'},{name: 'Hide Notes'}]);
+
 		
 		var configButton = $('<input type="button" id="docs-config" />')
 			.appendTo(docManager.header.find('#header-nav'))

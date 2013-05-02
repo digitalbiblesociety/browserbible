f6753b9a1efa7ad6c2c8e2c59446263dbb29ceb4
Jonathan Bitgood <jonbitgood@yahoo.com>
2013-05-02 13:53:42 -0500

    Added SVG icons

--- app/js/docs/docs.localizer.js
+++ app/js/docs/docs.localizer.js
@@ -37,6 +37,7 @@ docs.Languages = {
 		"plugin_config_option_wordsofchrist": "Words of Christ",
 		"plugin_config_option_notes": "Notes",
 		"plugin_config_option_images": "Images",
+		"plugin_config_option_maps": "Maps",
 		"plugin_config_option_video": "Video",
 		
 		"plugin_morphology_title": "Morphology Highlighting",
@@ -59,42 +60,5 @@ docs.Languages = {
 		"bible_navigator_chapters": "Chapters",
 		"plugin_config_option_titles": "Titles"
 
-	},
-	"ar": {
-		"site_title": "الكتاب المقدس متصفح",
-		"site_footer": "<a href='http://dbsbible.org/'>Sofia Bible Browser</a> مدعوم منم ",
-		
-		"plugin_config_title": "تكوين",
-		"plugin_config_option_theme": "موضوع",
-		"plugin_config_option_font": "الخط",
-		"plugin_config_option_size": "الحجم",
-		"plugin_config_option_chapters": "الفصول",
-		"plugin_config_option_verses": "آيات",
-		"plugin_config_option_wordsofchrist": "كلمات المسيح",
-		"plugin_config_option_notes": "ملاحظات",
-		"plugin_config_option_images": "صور",
-		"plugin_config_option_video": "الفيديو",
-		
-		"plugin_morphology_title": "تسليط الضوء على علم الصرف",
-		"plugin_config_table_strong": " قوي و# ,",
-		"plugin_config_table_morph": "علم الصرف / تردد",
-		"plugin_config_table_style": "ستايل",
-		"plugin_config_table_color": "لون",
-		"plugin_config_table_add": "إضافة صف",
-		
-		"plugin_lemma_title": "بيانات يما",
-		
-		"plugin_images_title": "صور",
-		
-		"plugin_video_title": "الفيديو",
-		
-		"plugin_notesandcf_note": "علما",
-		"plugin_notesandcf_cf": "المراجع الصليب",
-		
-		"bible_navigator_books": "الكتب",
-		"bible_navigator_chapters": "الفصول"
-		
-		
 	}
 }
-

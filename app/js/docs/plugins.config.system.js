48e90a229f5feeafd55623e9b7ad8cbe335b39a0
John Dyer <johndyer@gmail.com>
2013-05-10 09:37:46 -0500

    Added system level summary for links, etc.

--- app/js/docs/plugins.config.system.js
+++ app/js/docs/plugins.config.system.js
@@ -9,6 +9,9 @@ docs.plugins.push({
 
 	init: function(docManager) {
 
+		$('<div><p>inScript Bible Browser: <a href="http://www.github.com/digitalbiblesociety/biblebrowser/">source code</a> | <a href="http://www.github.com/digitalbiblesociety/biblebrowser/">Digital Bible Society</a></p></div>')
+			.appendTo(docManager.configWindow.content);
+
 			
 	}
 	
